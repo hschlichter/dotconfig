2bc570ddf3f33bdd50de08910cba6ca9688b3a88
Henrik Schlichter <henrik.schlichter@gmail.com>
2024-02-20 16:50:40 +0100

    changed colors in flash

--- nvim/lua/plugins/flash.lua
+++ nvim/lua/plugins/flash.lua
@@ -2,7 +2,6 @@ return {
     "folke/flash.nvim",
     event = "VeryLazy",
     opts = {},
-    -- stylua: ignore
     keys = {
         { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
         { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
@@ -10,4 +9,8 @@ return {
         { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
         { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
     },
+    config = function ()
+        vim.api.nvim_set_hl(0, "FlashCurrent", { fg = "#ff0000" });
+        vim.api.nvim_set_hl(0, "FlashCursor", { fg = "#00ff00" });
+    end,
 }

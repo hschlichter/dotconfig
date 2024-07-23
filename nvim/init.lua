5b2c2d36ab4622336b7468b9a73bf25145fc69c5
Henrik Schlichter <henrik.schlichter@gmail.com>
2024-07-23 15:28:08 +0200

    command mode support for C-a,C-e, etc.

--- nvim/init.lua
+++ nvim/init.lua
@@ -14,12 +14,17 @@ vim.opt.rtp:prepend(lazypath);
 vim.g.loaded_netrw = 1;
 vim.g.loaded_netrwPlugin = 1;
 
-vim.g.mapleader= " ";
+vim.g.mapleader = " ";
 
 local modes = {"n", "i", "v", "x"}
 for _, mode in ipairs(modes) do
-    vim.api.nvim_set_keymap(mode, "<C-c>", "<Esc>", { noremap = true, silent = true })
+    vim.api.nvim_set_keymap(mode, "<C-c>", "<Esc>", { noremap = true, silent = true });
 end
+
+vim.cmd("cnoremap <C-a> <Home>");
+vim.cmd("cnoremap <C-e> <End>");
+vim.cmd("cnoremap <C-f> <Right>");
+vim.cmd("cnoremap <C-b> <Left>");
 
 vim.keymap.set("n", "<C-b>", ":b#<CR>");
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv");
@@ -27,8 +32,8 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv");
 vim.keymap.set("n", "<C-d>", "<C-d>zz");
 vim.keymap.set("n", "<C-u>", "<C-u>zz");
 
-vim.keymap.set("n", "<leader>p", "\"_dP");
-vim.keymap.set("x", "<leader>p", "\"_dP");
+vim.keymap.set("n", "<leader>p", "\"+p");
+vim.keymap.set("x", "<leader>p", "\"+p");
 vim.keymap.set("n", "<leader>y", "\"+y");
 vim.keymap.set("v", "<leader>y", "\"+y");
 

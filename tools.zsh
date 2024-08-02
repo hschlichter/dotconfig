3c5e3fc1cec705513e5b6b82744957012ffd4eec
Henrik Schlichter <henrik.schlichter@gmail.com>
2024-08-02 15:35:25 +0200

    tools install script

---
+++ tools.zsh
@@ -0,0 +1,28 @@
+#!/bin/zsh
+
+if [[ -f "/opt/homebrew/bin/brew" ]] then
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+else
+    echo "Homebrew is not installed. Please install Homebrew first."
+    exit 1
+fi
+
+ensure_installed() {
+    if ! brew list --formula "$1" &> /dev/null; then
+        echo "Installing $1..."
+        brew install "$1"
+    else
+        echo "$1 is already installed."
+    fi
+}
+
+ensure_installed alacritty
+ensure_installed tmux
+ensure_installed zoxide
+ensure_installed bat
+ensure_installed fd
+ensure_installed ripgrep
+ensure_installed fzf
+ensure_installed yazi
+ensure_installed jq
+ensure_installed btop

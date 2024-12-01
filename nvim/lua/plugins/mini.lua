e745652dd99ac853b1764189c216de31e5c05c36
Henrik Schlichter <henrik.schlichter@gmail.com>
2024-12-01 10:53:38 +0100

    Added mini.jump

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -14,19 +14,19 @@ return {
                     sort = nil,
                 },
                 mappings = {
-                    close       = 'q',
-                    go_in       = 'l',
-                    go_in_plus  = 'L',
-                    go_out      = 'h',
-                    go_out_plus = 'H',
+                    close       = "q",
+                    go_in       = "l",
+                    go_in_plus  = "L",
+                    go_out      = "h",
+                    go_out_plus = "H",
                     mark_goto   = "'",
-                    mark_set    = 'm',
-                    reset       = '<BS>',
-                    reveal_cwd  = '@',
-                    show_help   = 'g?',
-                    synchronize = '=',
-                    trim_left   = '<',
-                    trim_right  = '>',
+                    mark_set    = "m",
+                    reset       = "<BS>",
+                    reveal_cwd  = "@",
+                    show_help   = "g?",
+                    synchronize = "=",
+                    trim_left   = "<",
+                    trim_right  = ">",
                 },
                 options = {
                     permanent_delete = true,
@@ -50,21 +50,17 @@ return {
                 {
                     custom_textobjects = nil,
                     mappings = {
-                        around = 'a',
-                        inside = 'i',
-
-                        around_next = 'an',
-                        inside_next = 'in',
-                        around_last = 'al',
-                        inside_last = 'il',
-
-                        goto_left = 'g[',
-                        goto_right = 'g]',
+                        around = "a",
+                        inside = "i",
+                        around_next = "an",
+                        inside_next = "in",
+                        around_last = "al",
+                        inside_last = "il",
+                        goto_left = "g[",
+                        goto_right = "g]",
                     },
-
                     n_lines = 50,
-                    search_method = 'cover_or_next',
-
+                    search_method = "cover_or_next",
                     silent = false,
                 }
             })
@@ -76,29 +72,41 @@ return {
         config = function ()
             require("mini.surround").setup({
                 custom_surroundings = nil,
-
                 highlight_duration = 500,
-
                 mappings = {
-                    add = 'sa',
-                    delete = 'sd',
-                    find = 'sf',
-                    find_left = 'sF',
-                    highlight = 'sh',
-                    replace = 'sr',
-                    update_n_lines = 'sn',
-
-                    suffix_last = 'l',
-                    suffix_next = 'n',
+                    add = "sa",
+                    delete = "sd",
+                    find = "sf",
+                    find_left = "sF",
+                    highlight = "sh",
+                    replace = "sr",
+                    update_n_lines = "sn",
+                    suffix_last = "l",
+                    suffix_next = "n",
                 },
-
                 n_lines = 20,
-
                 respect_selection_type = false,
-
-                search_method = 'cover',
-
+                search_method = "cover",
                 silent = false,
+            })
+        end
+    },
+    {
+        "echasnovski/mini.jump",
+        version = false,
+        config = function ()
+            require("mini.jump").setup({
+                mappings = {
+                    forward = 'f',
+                    backward = 'F',
+                    forward_till = 't',
+                    backward_till = 'T',
+                    repeat_jump = ';',
+                },
+                delay = {
+                    highlight = 250,
+                    idle_stop = 10000000,
+                }
             })
         end
     }
